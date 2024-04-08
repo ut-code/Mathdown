ee8625b1fbb200a0a6ed24792946dab3b0194a40
aster <137767097+aster-void@users.noreply.github.com>
2024-04-09 00:57:40 +0900

    [NW] got the basic functions working, but the tippy won't work

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Tippy from "@tippyjs/react";
 import markdownLink from "/hoge.md?url";
 import { ExtractDefinitions } from "./MDToDefinitions";
 import { MDToHTML } from "./MDToHTML";
-import { replaceMathjsToTex } from "./mathjs-tex";
+import { replaceExternalSyntax } from "./external-syntax";
 
 import "katex/dist/katex.min.css";
 import "tippy.js/dist/tippy.css";
@@ -16,31 +16,51 @@ import "tippy.js/dist/tippy.css";
 export default function App() {
   const [markdown, setMarkdown] = useState("");
   const [html, setHTML] = useState("");
+  const [dict, setDict] = useState(new Map());
   const opts = {
     prefix: "!define",
     suffix: "!enddef",
   };
 
+  // get markdown
   useEffect(() => {
     fetch(markdownLink)
       .then((res) => res.text())
       .then((t) => setMarkdown(t))
       .catch((err) => console.error("Error fetching Hoge.md:", err));
   }, []);
-  useEffect(() => {
-    // MDtoHTML is async for some reason.
-    markdown
-    MDToHTML(.replaceAll(opts.prefix, "##").replaceAll(opts.suffix, ""))
-      .then((h) => setHTML(h))
-      .catch(console.error);
-  });
 
-  const dict = ExtractDefinitions(hogeMd, opts.prefix, opts.suffix);
+  // use markdown (separation is necessary because it's async)
+  useEffect(() => void insideUseEffect(), [markdown]);
+  async function insideUseEffect() {
+    // prepare dictionary
+    let d = ExtractDefinitions(markdown, opts.prefix, opts.suffix);
+    const newd = new Map<string, string>();
+    const promises = [];
+    d.forEach((k, v) => {
+      let md = replaceExternalSyntax(v);
+      md = md.replaceAll(opts.prefix, "##").replaceAll(opts.suffix, "");
+      const p = MDToHTML(md).then((newv) => newd.set(k, newv));
+      promises.push(p);
+    });
+    Promise.all(promises).then(() => setDict(newd));
+
+    // prepare HTML
+    var md;
+    try {
+      md = replaceExternalSyntax(markdown);
+    } catch (e) {
+      md = e.toString();
+    }
+    MDToHTML(md.replaceAll(opts.prefix, "##").replaceAll(opts.suffix, ""))
+      .then((h) => setHTML(h))
+      .catch(() => "MDToHTML failed");
+  }
 
   return <ConvertMarkdown dictionary={dict} html={html} opts={opts} />;
 }
 
-// this uses given markdown as the source to extract definition from,
+// this uses given dictionary as the source to extract definition from,
 // and given html to render the main note.
 function ConvertMarkdown({
   dictionary,
@@ -51,7 +71,6 @@ function ConvertMarkdown({
   html: string;
   opts: { prefix: string; suffix: string };
 }) {
-
   let parsing = html.split("\n");
 
   // this is O(n**2). reduce the order if you can.
@@ -62,37 +81,34 @@ function ConvertMarkdown({
       // I hard-coded the assumption that a definition will turn into h2. if you got any better way to do this, do that.
       if (!line.includes(`<h2>${word}</h2>`)) {
         // make sure ${word} is the first attribute of class; otherwise the word replacement below will fail.
-        parsing[idx] = line.replaceAll(word, `<span class="${word} underline">${word}</span>`);
+        parsing[idx] = line.replaceAll(
+          word,
+          `<span class="${word} underline">${word}</span>`,
+        );
         // nested span tag to underline targeted dictionary. it doesn't work well with <span class="${word}, underline">. If there are better way, fix it.
-	//   there are two reasons it didn't work:
-	//   - class divider is " ", not ", "
-	//   - domNode.attribs.class will be `${word} underline` so it doesn't match word
       }
       idx++;
     }
   });
   let parsedHtml = parsing.join("\n");
-  console.log(parsedHtml);
 
   const options: HTMLReactParserOptions = {
     replace(domNode) {
       // domNode の最初の class 属性を取り出す。 (indexError でなく undefined になるため、[0] は安全)
       const word: string | undefined = domNode.attribs?.class?.split(" ")[0];
       // HTML 的には多分動くが、気持ち悪いので最初の class 属性 = word を排除
-      const newClass: string = domNode.attribs?.class?.split(" ").slice(0).join(" ");
+      const newClass: string = domNode.attribs?.class
+        ?.split(" ")
+        .slice(0)
+        .join(" ");
       // 与えられたノードが Element であり、その class 属性が undefined または空文字列でなく、 dictionary 内のいずれかの単語と一致するかどうかを確認
-      if (domNode instanceof Element && domNode.attribs?.class && dictionary.has(word)) {
+      if (
+        domNode instanceof Element &&
+        domNode.attribs?.class &&
+        dictionary.has(word)
+      ) {
         return (
-          <Tippy
-            content={
-              <Markdown
-                rehypePlugins={[rehypeKatex]}
-                remarkPlugins={[remarkMath]}
-              >
-                {dictionary.get(word)}
-              </Markdown>
-            }
-          >
+          <Tippy content={dictionary.get(word)}>
             <span className={newClass}>{word}</span>
           </Tippy>
         );
---
+++ src/external-syntax.ts
@@ -0,0 +1,103 @@
+import * as math from "mathjs";
+
+/* example syntax (mathjs): 
+input:
+
+!define ピタゴラスの定理
+
+$$
+c = sqrt(a**2 + b**2)
+$$
+
+output:
+
+!define ピタゴラスの定理
+
+$$
+c = \sqrt{a^{2} + b^{2}}
+$$
+
+*/
+type markdown = string;
+type AvailableSyntaxes = 
+  | "mathjs" // mathjs-powered simplified TeX
+  | "math" // literal KaTeX
+  | "unspecified"; // default value; (must be) impossible to reach
+
+// WARNING: this function may fail if m contains invalid syntax \ invalid syntax type (not in the AvailableSyntaxes)
+export function replaceExternalSyntax(m: markdown): markdown {
+  let result = "";
+  let blockModeLines = [];
+  let syntaxType: AvailableSyntaxes = "unspecified";
+  let isBlockMode = false;
+  let lines = 0;
+  for (const line of m.split("\n")) {
+    ++lines;
+    if (isBlockMode) {
+      blockModeLines.push(line);
+    } else if (!line.startsWith("```")){
+      result += line + "\n";
+    }
+    if (line.startsWith("```")) {
+      isBlockMode = !isBlockMode;
+      if (isBlockMode) {
+        // start of block mode;
+        const syntaxSpecifier = line.slice(3).trim();
+        switch (syntaxSpecifier) {
+          case "math":
+            syntaxType = "math";
+            break;
+          case "mathjs":
+            syntaxType = "mathjs";
+            break;
+          case "":
+            throw new Error(
+              `Please Specify a syntax specifier on line ${lines}`,
+            );
+          default:
+            throw new Error(
+              `Unrecognized Syntax Specifier: ${syntaxSpecifier} on line ${lines}`,
+            );
+        }
+      } else {
+        // end of block mode;
+        if (line.trim() !== "```") {
+          throw new Error(`Invalid syntax: expected ${"```"} at line ${lines}`);
+        }
+        blockModeLines.pop(); // get rid of the last ```
+        const block = blockModeLines.join("\n");
+        result += handle(block, syntaxType);
+        blockModeLines = [];
+      }
+    } else if (line.includes("{math}") && line.includes("{}")) {
+      // TODO: complete this inline functionality
+    }
+  }
+  return result;
+}
+
+function handle(block: string, syntaxType: AvailableSyntaxes) {
+  switch (syntaxType) {
+    case "math":
+      return KaTeX(block);
+    case "mathjs":
+      return MathJS(block);
+    default:
+      throw new Error(`Unknown Syntax Type: ${syntaxType}`);
+  }
+}
+
+type mathjs = string;
+type tex = string;
+function MathJS(m: mathjs): tex {
+  return "```math\n" + math.parse(m).toTex() + "\n```\n";
+}
+function KaTeX(b: block): tex {
+  return "```math\n" + b + "\n```\n";
+}
+
+function assert(b: bool, text: string = "Explicit assertion failed.") {
+  if (!b) {
+    throw new Error(text);
+  }
+}
